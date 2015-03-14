6c7625949b50e46840efe8ab13b04ac410e03563
unknown <mike.bedney@gmail.com>
2015-03-13 19:09:13 -0700

    Upgraded simple_mvc

--- exercises/simple_mvc.py
+++ exercises/simple_mvc.py
@@ -48,19 +48,29 @@ class Application():
 app = Application()
 
 # define models (
-app.models["user"] = Model("user", ["name", "score"])
+app.models["user"] = Model("user", ["name", "score", "age"])
 app.models["game"] = Model("game", ["game_name", "description"])
 
 # load model objects form database tables
 app.models["user"].objects = [
-    {"name": "Bob", "score": "9"},
-    {"name": "Carol", "score": "11"},
-    {"name": "Ted", "score": "15"},
-    {"name": "Alice", "score": "13"}]
+    {"name": "Bob", "score": "9", "age": "Old-as-shit"},
+    {"name": "Carol", "score": "11", "age": "Not-as-old"},
+    {"name": "Ted", "score": "15", "age": "itty-bitty"},
+    {"name": "Alice", "score": "13", "age": "Semi-old"}]
+
+template = "\nHello <em>{{name}}</em>, your score is <strong>{{score}}</strong>, your age is <strong>{{age}}</strong>.<br>\n"
+template_results = "\nHello <em>{{name}}</em>, your rank is <strong>{{score}}</strong>.<br>\n"
+view = View(template, app.models["user"])
+view_results = View(template_results, app.models["user"])
 
 app.controller.routes = {
-    "/scores/": View("\nHello <em>{{name}}</em>, your score is <strong>{{score}}</strong>.<br>\n", app.models["user"])
+    "/scores/": view,
+    "/results/": view_results
 }
+
+request_path = "/results/"
+print(app.controller.route(request_path))
+
 
 request_path = "/scores/"
 print(app.controller.route(request_path))
