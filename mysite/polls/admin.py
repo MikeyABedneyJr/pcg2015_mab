a155159392a242f636f24a269063843512073ccb
unknown <mike.bedney@gmail.com>
2015-03-16 21:23:41 -0700

    Last push of the night

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
 # Register your models here.
+admin.site.register(Choice)
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
 
 admin.site.register(Question, QuestionAdmin)
