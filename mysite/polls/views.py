1be0a02db60ff989db64b131e79ad0492b1d65ff
unknown <mike.bedney@gmail.com>
2015-03-23 20:31:26 -0700

    working even better now

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -31,5 +31,12 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    
